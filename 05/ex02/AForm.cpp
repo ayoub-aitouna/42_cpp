00a569121b074538a515fe3b0ac762c1124c94f1
jrayoub <aitounaayoub05@gmail.com>
2023-07-22 12:01:51 +0100

    changes

--- 05/ex02/AForm.cpp
+++ 05/ex02/AForm.cpp
@@ -1,24 +1,24 @@
-#include "Form.hpp"
+#include "AForm.hpp"
 #include "Bureaucrat.hpp"
 
-Form::Form() : name(""), singed(false), requredGradeToSign(1), requiredGradeToExecute(1)
+AForm::AForm() : name(""), singed(false), requredGradeToSign(1), requiredGradeToExecute(1)
 {
 }
 
-Form::Form(const Form &lhs) : requredGradeToSign(1), requiredGradeToExecute(1)
+AForm::AForm(const AForm &lhs) : requredGradeToSign(1), requiredGradeToExecute(1)
 {
 	*this = lhs;
 }
 
-Form::Form(std::string name, int requredGradeToSign, int requiredGradeToExecute) : name(name), singed(false), requredGradeToSign(requredGradeToSign), requiredGradeToExecute(requiredGradeToExecute)
+AForm::AForm(std::string name, int requredGradeToSign, int requiredGradeToExecute) : name(name), singed(false), requredGradeToSign(requredGradeToSign), requiredGradeToExecute(requiredGradeToExecute)
 {
 	if (requiredGradeToExecute < 1 || requredGradeToSign < 1)
-		throw Form::GradeTooHighException();
+		throw AForm::GradeTooHighException();
 	if (requiredGradeToExecute > 150 || requredGradeToSign > 150)
-		throw Form::GradeTooLowException();
+		throw AForm::GradeTooLowException();
 }
 
-Form &Form::operator=(const Form &lhs)
+AForm &AForm::operator=(const AForm &lhs)
 {
 	if (this != &lhs)
 	{
@@ -30,32 +30,32 @@ Form &Form::operator=(const Form &lhs)
 	return (*this);
 }
 
-std::string Form::getName() const
+std::string AForm::getName() const
 {
 	return (this->name);
 }
 
-bool Form::getSigned() const
+bool AForm::getSigned() const
 {
 	return (this->singed);
 }
 
-int Form::getRequredGradeToSign() const
+int AForm::getRequredGradeToSign() const
 {
 	return (this->requredGradeToSign);
 }
 
-int Form::getRequiredGradeToExecute() const
+int AForm::getRequiredGradeToExecute() const
 {
 	return (this->requiredGradeToExecute);
 }
 
-bool Form::beSigned(Bureaucrat &bureaucrat)
+bool AForm::beSigned(Bureaucrat &bureaucrat)
 {
 	return (this->singed = bureaucrat.getGrade() <= this->requredGradeToSign);
 }
 
-std::ostream &operator<<(std::ostream& os, const Form& value)
+std::ostream &operator<<(std::ostream& os, const AForm& value)
 {
 	os	<< value.getName() << ", " << (value.getSigned() ? "" : "not ") 
 			<< "singed, required Grade to Sign is " << value.getRequredGradeToSign()
@@ -65,6 +65,6 @@ std::ostream &operator<<(std::ostream& os, const Form& value)
 }
 
 
-Form::~Form()
+AForm::~AForm()
 {
 }
---
+++ 05/ex02/AForm.hpp
@@ -0,0 +1,49 @@
+#ifndef AFORM_HPP
+#define AFORM_HPP
+# include <exception>
+# include <string>
+# include <iostream>
+
+class Bureaucrat;
+class AForm
+{
+  public:
+	AForm();
+	AForm(const AForm &lhs);
+	AForm(std::string name, int requredGradeToSign, int requiredGradeToExecute);
+	AForm &operator=(const AForm &lhs);
+	~AForm();
+
+  private:
+	const std::string name;
+	bool singed;
+	const int requredGradeToSign;
+	const int requiredGradeToExecute;
+
+	public:
+		std::string getName() const;
+		bool getSigned() const;
+		int getRequredGradeToSign() const;
+		int getRequiredGradeToExecute() const;
+		bool beSigned(Bureaucrat& bureaucrat);
+		
+  public:
+	class GradeTooHighException : public std::exception
+	{
+	  public:
+		const char *what() const throw()
+		{
+			return "too high grade";
+		}
+	};
+	class GradeTooLowException : public std::exception
+	{
+	  public:
+		const char *what() const throw()
+		{
+			return "too low grade";
+		}
+	};
+};
+std::ostream &operator<<(std::ostream& os, const AForm& value);
+#endif
