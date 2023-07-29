4d7b46a579407829f7340428b609e78e640c4d89
jrayoub <aitounaayoub05@gmail.com>
2023-07-29 20:46:03 +0100

    changes

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -1,4 +1,5 @@
 #include "ScalarConverter.hpp"
+#include <cstdlib>
 
 ScalarConverter::ScalarConverter()
 {
@@ -6,30 +7,101 @@ ScalarConverter::ScalarConverter()
 
 ScalarConverter::ScalarConverter(const ScalarConverter &lhs)
 {
-    *this = lhs;
+	*this = lhs;
 }
 ScalarConverter &ScalarConverter::operator=(const ScalarConverter &lhs)
 {
-    (void)lhs;
-    return (*this);
+	(void)lhs;
+	return (*this);
+}
+
+bool	is_number(char c)
+{
+	return (c > '0' && c < '9');
+}
+bool	is_all_num(std::string str, int size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		if (!is_number(str.at(i)))
+			return (false);
+	}
+	return (true);
+}
+
+bool	float_type_validator(std::string str)
+{
+	int	dot_index;
+	int	f_index;
+
+	dot_index = str.find_first_of('.');
+	f_index = str.find_first_of('f');
+	if (dot_index == -1 || f_index == -1 || (f_index - dot_index) != 1)
+		return (false);
+	else
+		return (is_all_num(str, dot_index));
+}
+
+bool	double_type_validator(std::string str)
+{
+	int	f_index;
+
+	f_index = str.find_first_of('f');
+	if (f_index == -1)
+		return (false);
+	else
+		return (is_all_num(str, f_index));
+}
+
+int	get_type(std::string str)
+{
+	if (str.length() == 1)
+		return (T_CHAR);
+	if (float_type_validator(str))
+		return (T_FLOAT);
+	if (double_type_validator(str))
+		return (T_DOUBLE);
+	if (is_all_num(str, str.length()))
+		return (T_DOUBLE);
+	return (T_NPOS);
+}
+
+void	print_result(void)
+{
 }
 
 void ScalarConverter::convert(std::string initial_value)
 {
-    float f_value;
-    double d_value;
-    int i_value;
-    char c_value;
+	int	type;
 
-    f_value = atof(initial_value.c_str());
-    d_value = atof(initial_value.c_str());
-    i_value = atoi(initial_value.c_str());
-    c_value = static_cast<char>(i_value);
-
-    std::cout << "f : " << f_value << std::endl;
-    std::cout << "d : " << d_value << std::endl;
-    std::cout << "i : " << i_value << std::endl;
-    std::cout << "c : " << c_value << std::endl;
+	type = get_type(initial_value);
+	switch (type)
+	{
+	case T_CHAR:
+		c_value = static_cast<char>(initial_value.at(0));
+		i_value = static_cast<int>(c_value);
+		f_value = static_cast<float>(c_value);
+		d_value = static_cast<double>(c_value);
+		break ;
+	case T_INT:
+		i_value = atoi(initial_value.c_str());
+		c_value = static_cast<int>(i_value);
+		f_value = static_cast<float>(i_value);
+		d_value = static_cast<double>(i_value);
+		break ;
+	case T_FLOAT:
+		f_value = atof(initial_value.c_str());
+		c_value = static_cast<int>(f_value);
+		i_value = static_cast<float>(f_value);
+		d_value = static_cast<double>(f_value);
+		break ;
+	case T_DOUBLE:
+		f_value = atof(initial_value.c_str());
+		break ;
+	case T_NPOS:
+		break ;
+	}
+	print_result();
 }
 
 ScalarConverter::~ScalarConverter()
--- 06/ex00/ScalarConverter.hpp
+++ 06/ex00/ScalarConverter.hpp
@@ -4,18 +4,31 @@
 #include <iostream>
 #include <cstdlib>
 
+typedef enum type
+{
+	T_NPOS = -1,
+	T_CHAR = 0,
+	T_INT = 1,
+	T_FLOAT = 2,
+	T_DOUBLE = 3
+} t_type;
+
 class ScalarConverter
 {
-private:
-    /* data */
-public:
-    ScalarConverter();
-    ScalarConverter(const ScalarConverter &lhs);
-    ScalarConverter &operator=(const ScalarConverter &lhs);
-    ~ScalarConverter();
+	private:
+		static float f_value;
+		static double d_value;
+		static int i_value;
+		static char c_value;
 
-public:
-    static void convert(std::string initial_value);
+	public:
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter &lhs);
+		ScalarConverter &operator=(const ScalarConverter &lhs);
+		~ScalarConverter();
+
+	public:
+		static void convert(std::string initial_value);
 };
 
 #endif
